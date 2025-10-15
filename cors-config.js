be7c91465aae6c113bc7da577f22307ff56e54d6
YoungjinSeo <piggy0513@ajou.ac.kr>
2025-10-16 03:14:21 +0900

    ...

--- cors-config.js
+++ cors-config.js
@@ -7,7 +7,8 @@ const getCorsConfig = () => {
     'http://localhost:5173',    // 로컬 Vite 개발 서버
     'http://localhost:3000',    // 로컬 React 개발 서버 (백업)
     'http://127.0.0.1:5173',
-    'http://127.0.0.1:5500'    // 로컬 Vite (IP 주소)
+    'http://127.0.0.1:5500',
+    'https://pwd-week6-bbqjam05.netlify.app/',
   ];
 
   // 환경변수에서 추가 URL들 가져오기
